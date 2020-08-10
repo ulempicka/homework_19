479ce59e55675f72c24d5bd50bf01f1ab262e9f8
ulempicka <urszula.lempicka@gmail.com>
2020-08-10 16:49:34 +0200

    commit 7 - updated according to comments, finished last comment but solution is not ideal...

--- src/main/java/euler2/EulerTwo.java
+++ src/main/java/euler2/EulerTwo.java
@@ -13,6 +13,9 @@ public class EulerTwo {
             fibonacci1 = fibonacci2;
             fibonacci2 = fibonacci3;
 
+            if (fibonacci3 >= limit){
+                return sum;
+            }
             if (fibonacci3 % 2 == 0) {
                 System.out.println(fibonacci3);
                 sum += fibonacci3;
